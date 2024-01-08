0c1fb426bf580ee7728d2ae32def6eedf1847035
samlefaure <156022958+samlefaure@users.noreply.github.com>
2024-01-08 22:20:54 +0100

    Add files via upload

---
+++ site_devoir/premier.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>JavaScript</title>
+</head>
+<body>
+<script language="JavaScript"
+ type="text/JavaScript">
+ var s = document.createElement("script");
+ s.setAttribute("type", "text/JavaScript");
+ s.setAttribute("language", "JavaScript");
+ s.setAttribute("src", "script.js");
+ document.getElementsByTagName("head")[0].appendChild(s);
+</script>
+</body>
+</html>
